c72333b7ef6992aedecac0b4bec93b73efa667cf
Robert-Wilson Peremobowei Favour <103349217+Favour-Pere@users.noreply.github.com>
2025-05-29 03:39:19 +0100

    Refactor LoginPage to use StatefulWidget; enhance UI with email and password fields, and implement login functionality; update WelcomePage navigation to use push instead of pushReplacement

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -1,8 +1,26 @@
+import 'package:flutt_app/views/widget_tree.dart';
 import 'package:flutt_app/widgets/hero_widget.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController controllerEmail = TextEditingController();
+  TextEditingController controllerPassword = TextEditingController();
+  String confirmedEmail = '123';
+  String confirmedPassword = '456';
+
+  @override
+  void dispose() {
+    controllerEmail.dispose();
+    controllerPassword.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +28,74 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(children: [HeroWidget(title: 'Login')]),
+        child: Column(
+          children: [
+            HeroWidget(title: 'Login'),
+            SizedBox(height: 20),
+            TextField(
+              controller: controllerEmail,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                hintText: 'Enter your email',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+              ),
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+            SizedBox(height: 20),
+
+            TextField(
+              controller: controllerPassword,
+              obscureText: true,
+              autocorrect: false,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                hintText: 'Enter your password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+              ),
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+            SizedBox(height: 50),
+            FilledButton(
+              onPressed: () {
+                onLoginPressed();
+              },
+
+              style: FilledButton.styleFrom(
+                minimumSize: Size(double.infinity, 50.0),
+              ),
+              child: Text('Login'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void onLoginPressed() {
+    if (confirmedEmail == controllerEmail.text &&
+        confirmedPassword == controllerPassword.text) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return WidgetTree();
+          },
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Invalid email or password')));
+      controllerEmail.clear();
+      controllerPassword.clear();
+    }
   }
 }
